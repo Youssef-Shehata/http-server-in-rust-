b54ae55d441d042549a2a4910fd710bbfcfb7599
joe <31982488+Youssef-Shehata@users.noreply.github.com>
2024-07-13 21:41:52 +0300

    checking a valid path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
- use std::{io::{Write}, net::TcpListener};
+ use std::{io::{Read, Write}, net::TcpListener};
 
 
 fn main() {
@@ -10,10 +10,27 @@ fn main() {
     //
      let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
+     let mut buffer = [0;1024];
      for stream in listener.incoming() {
        match stream {
+
            Ok(mut _stream) => {
-               _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
+               let _ = _stream.read(&mut buffer);
+               let req = String::from_utf8_lossy(&buffer[..]);
+               
+               if  let Some(path )= extract_path(&req){
+                   if path == "/"{
+                       _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
+
+
+                   }
+                   
+                   else{
+                        _stream.write(b"HTTP/1.1 404 NOT FOUND\r\n\r\n").expect("404 \n");
+
+                   }
+
+           }
            }
 
            Err(e) => {
@@ -22,4 +39,15 @@ fn main() {
        }
     }
 
+}
+
+
+fn extract_path(req: &str) -> Option<&str>{
+   
+     let req_lines: Vec<&str>  =  req.lines().collect();
+     let req_line = req_lines[0];
+     let req_parts : Vec<&str> = req_line.split_whitespace().collect();
+     
+     println!("request : {}", req_parts[1]);
+     Some(req_parts[1])
 }
