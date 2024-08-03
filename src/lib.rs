5e7a0a43a56bd314d86f1703ba5ebdad6df79ddb
joe <31982488+Youssef-Shehata@users.noreply.github.com>
2024-08-03 08:20:15 +0300

    multithreading the server

---
+++ src/lib.rs
@@ -0,0 +1,76 @@
+use std::{
+    option,
+    sync::{mpsc, Arc, Mutex},
+    thread::{self},
+};
+pub struct Thread_pool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
+}
+
+type Job = Box<dyn FnOnce() + 'static + Send>;
+impl Thread_pool {
+    pub fn new(size: usize) -> Thread_pool {
+        assert!(size > 0);
+        let mut workers = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        Thread_pool {
+            workers,
+            sender: Some(sender),
+        }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + 'static + Send,
+        F: Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for Thread_pool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in &mut self.workers {
+            if let Some(t) = worker.thread.take() {
+                t.join().unwrap();
+            }
+        }
+    }
+}
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let builder = thread::Builder::new();
+        let thread = match builder.spawn(move || loop {
+            match receiver.lock().unwrap().recv() {
+                Ok(j) => {
+                    println!("Worker {id} has recieved a job ; and he is working on it ..");
+                    j();
+                }
+                Err(_) => {
+                    eprint!("thread dying with dignity");
+                    break;
+                }
+            };
+        }) {
+            Ok(t) => Some(t),
+            Err(e) => {
+                eprintln!("coudlnt spawn a new thread");
+                None
+            }
+        };
+        Worker { id, thread }
+    }
+}
